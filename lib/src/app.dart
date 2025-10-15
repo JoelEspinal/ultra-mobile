6d2746990fb84a30f5f9b67519c37cb99c9572ba
JoelEspinal <j.espinalcarrasco@gmail.com>
2025-10-15 14:13:15 -0400

    Declare ApiClient and Todo service as providers

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:ultra_mobile/src/data/network/todos_api/api_client.dart';
+import 'package:ultra_mobile/src/data/network/todos_api/services/todo_service.dart';
+import 'package:ultra_mobile/src/presentation/home_screen/home_screen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Ultra Mobile Challenge',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const Scaffold(body: Center(child: Text('Hello, World!'))),
+    return MultiProvider(
+      providers: [
+        Provider<ApiClient>(create: (_) => ApiClient()),
+        Provider<TodoService>(
+          create: (context) =>
+              TodoService(apiClient: context.read<ApiClient>()),
+        ),
+      ],
+      child: MaterialApp(home: Scaffold(body: HomeScreen())),
     );
   }
 }
