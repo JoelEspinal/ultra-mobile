bd86bad3f7d1b5b2d6626af8219f5d703bb6a24d
JoelEspinal <j.espinalcarrasco@gmail.com>
2025-10-15 23:32:41 -0400

    Setup visual search bar

--- lib/src/presentation/home_screen/todos_page.dart
+++ lib/src/presentation/home_screen/todos_page.dart
@@ -13,21 +13,57 @@ class TodosPage extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         body: Stack(
-          children: <Widget>[
-            // 1. The main scrollable content
-            ListView.builder(
-              padding: const EdgeInsets.only(
-                top: 100,
-              ), // Push content down past the floating bar
-              itemCount: 50,
-              itemBuilder: (context, index) {
-                return ListTile(title: Text('Content Item $index'));
-              },
+          children: [
+            RefreshIndicator(
+              onRefresh: () => todosCubit.loadTodos(),
+              child: Center(
+                child: BlocBuilder<TodosCubit, TodosState>(
+                  builder: (context, state) {
+                    if (state.status == TodoStatus.loading) {
+                      return const CircularProgressIndicator();
+                    } else if (state.status == TodoStatus.failure) {
+                      return Text(state.errorMessage);
+                    } else if (state.status == TodoStatus.success) {
+                      return ListView.builder(
+                        itemCount: state.todos.length,
+                        itemBuilder: (context, index) {
+                          final todo = state.todos[index];
+                          return ListTile(
+                            title: Text(
+                              todo.todo,
+                              style: todo.completed
+                                  ? TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      decoration: TextDecoration.lineThrough,
+                                      decorationColor: Colors.black,
+                                      decorationThickness: 2.0,
+                                    )
+                                  : null,
+                            ),
+
+                            leading: IconButton(
+                              onPressed: () =>
+                                  todosCubit.toggleTodoStatus(todo.id),
+                              icon: Icon(
+                                todo.completed
+                                    ? Icons.check_box
+                                    : Icons.check_box_outline_blank,
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    } else {
+                      return const Text('Press the button to load todos.');
+                    }
+                  },
+                ),
+              ),
             ),
 
             // 2. The Floating Search Bar
             Positioned(
-              top: 20, // Distance from the top of the screen
+              top: 20,
               left: 15,
               right: 15,
               child: Container(
@@ -37,69 +73,26 @@ class TodosPage extends StatelessWidget {
                   borderRadius: BorderRadius.circular(12),
                   boxShadow: [
                     BoxShadow(
-                      color: Colors.black.withOpacity(0.2),
+                      color: Colors.black45,
                       blurRadius: 8,
                       offset: const Offset(0, 4),
                     ),
                   ],
                 ),
-                child: const TextField(
+                child: TextField(
                   decoration: InputDecoration(
-                    hintText: "Floating Search Bar...",
+                    hintText: " todo...",
                     border: InputBorder.none,
-                    prefixIcon: Icon(Icons.search),
+                    prefixIcon: IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.search),
+                    ),
                   ),
                 ),
               ),
             ),
           ],
         ),
-
-        // RefreshIndicator(
-        //   onRefresh: () => todosCubit.loadTodos(),
-        //   child: Center(
-        //     child: BlocBuilder<TodosCubit, TodosState>(
-        //       builder: (context, state) {
-        //         if (state.status == TodoStatus.loading) {
-        //           return const CircularProgressIndicator();
-        //         } else if (state.status == TodoStatus.failure) {
-        //           return Text(state.errorMessage);
-        //         } else if (state.status == TodoStatus.success) {
-        //           return ListView.builder(
-        //             itemCount: state.todos.length,
-        //             itemBuilder: (context, index) {
-        //               final todo = state.todos[index];
-        //               return ListTile(
-        //                 title: Text(
-        //                   todo.todo,
-        //                   style: todo.completed
-        //                       ? TextStyle(
-        //                           fontWeight: FontWeight.bold,
-        //                           decoration: TextDecoration.lineThrough,
-        //                           decorationColor: Colors.black,
-        //                           decorationThickness: 2.0,
-        //                         )
-        //                       : null,
-        //                 ),
-
-        //                 leading: IconButton(
-        //                   onPressed: () => todosCubit.toggleTodoStatus(todo.id),
-        //                   icon: Icon(
-        //                     todo.completed
-        //                         ? Icons.check_box
-        //                         : Icons.check_box_outline_blank,
-        //                   ),
-        //                 ),
-        //               );
-        //             },
-        //           );
-        //         } else {
-        //           return const Text('Press the button to load todos.');
-        //         }
-        //       },
-        //     ),
-        //   ),
-        // ),
       ),
     );
   }
