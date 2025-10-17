2920141a0dcd3cffe29e2359df5249afa2a08333
JoelEspinal <j.espinalcarrasco@gmail.com>
2025-10-17 14:13:56 -0400

    Add new method to persist data

--- lib/src/domain/repositories/persistence_repository.dart
+++ lib/src/domain/repositories/persistence_repository.dart
@@ -1,7 +1,7 @@
 import '../../domain/entities/todo.dart' as todo_entity;
 
 abstract class PersistenceRepository {
-  todo_entity.Todo? getTodo(int id);
+  Future<todo_entity.Todo?> getTodo(int id);
   Future<List<todo_entity.Todo>> getTodos();
   Future<void> saveTodo(todo_entity.Todo todo);
   Future<void> updateTodo(todo_entity.Todo todo);
