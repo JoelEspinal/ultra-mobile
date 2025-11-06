922702d8e235c743f62416dbc2cc7bbb26cdf1c6
JoelEspinal <j.espinalcarrasco@gmail.com>
2025-11-06 16:19:40 -0400

    Add toggle favorite use case

---
+++ lib/src/domain/use_cases/local_use_cases/toggle_favorite_todo_use_case.dart
@@ -0,0 +1,27 @@
+import 'package:dartz/dartz.dart';
+import '../../../common/failures/failure.dart';
+
+import '../../../data/database/hive/models/todo.dart';
+import '../../repositories/local_todo_repository.dart';
+
+class ToggleFavoriteTodoUseCase {
+  final LocalPersistenceRepository localPersistenceRepository;
+
+  ToggleFavoriteTodoUseCase({
+    required this.localPersistenceRepository,
+  });
+
+  Future<Either<Failure, Unit>> execute(int id) async {
+    Todo? currentTodo = await localPersistenceRepository.getTodo(id);
+    if (currentTodo == null) return Left(UnablToFindTodoFailire("$id"));
+
+    Todo? modelTodo = await localPersistenceRepository.getTodo(id);
+    if (modelTodo == null) throw UnablToFindTodoFailire("$id");
+
+    Todo resultTodo =
+        await localPersistenceRepository.toggleFavorite(modelTodo);
+    await localPersistenceRepository.updateTodo(resultTodo);
+
+    return Future.value(Right(unit));
+  }
+}
