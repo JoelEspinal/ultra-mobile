d888b335c95f8d43975c6da0bb3b7ef3a4cc5243
JoelEspinal <j.espinalcarrasco@gmail.com>
2026-01-17 18:15:02 -0400

    Fix NDK vertion

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.ultra_mobile"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.13113456"// flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -18,13 +18,6 @@ subprojects {
 subprojects {
     project.evaluationDependsOn(":app")
 }
-
-ndk {
-        // Set your desired NDK version (e.g., "25.1.9517409")
-        // or rely on the one installed in SDK Manager for simplicity
-        ndkVersion "28.2.13676358"
-    }
-    
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
